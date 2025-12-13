d7c89394ebe173fc4a5000a6d4fee3e52f66ef40
Daniel Sánchez-García <daniel.sanchezgarcia@uca.es>
2025-12-13 11:25:01 +0100

    0.7.6 - debugging apmv_setpoints.py

--- using apmv setpoints_v01.py
+++ using apmv setpoints_v01.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from besos import eppy_funcs as ef
 from besos import eplus_funcs as ep
-import accim.sim.apmv_setpoints_wip_2 as apmv
+import accim.sim.apmv_setpoints_wip_4_3_testing as apmv
+wip = '_wip_4_2'
 import os
 
 # Visualization setup
@@ -12,10 +13,12 @@ sns.set_style("whitegrid")
 
 # Define file paths
 # idfname = 'TestModel_onlyGeometryForVRFsystem_2zones_CalcVent_V940.idf'
-# idfname = 'TestModel_ALJARAFE CENTER_mod.idf'
-# idfname = 'TestModel_V940_VRFsystem_script_mod.idf'
-# idfname = 'TestModel_TestResidentialUnit_v01_VRF_2.idf'
-idfname = 'TestModel_TestResidentialUnit_v01_VRF_2_air-veloc-mod.idf'
+# idfname = 'TestModel_ALJARAFE CENTER_mod.idf'# not working yet, see zonename from apmv.get_available_target_names(building=building_pmv)
+idfname = 'TestModel_ALJARAFE CENTER_mod_zonelist.idf'
+
+
+# idfname = 'TestModel_TestResidentialUnit_v01_VRF_2_air-veloc-mod.idf' # only spacelist, no zonelist; not working yet
+# idfname = 'TestModel_TestResidentialUnit_v01_VRF_2_air-veloc-mod_zonelist-and-spacelist.idf'
 epwfile = "Seville.epw"
 
 # print(f"IDF File: {idfname}")
@@ -23,6 +26,9 @@ epwfile = "Seville.epw"
 
 # Load the building model using BESOS
 building = ef.get_building(idfname)
+
+zones = apmv._resolve_targets(building=building)
+print(zones)
 
 # Ensure zones are always occupied for the demonstration
 # apmv.set_zones_always_occupied(building=building)
@@ -37,31 +43,40 @@ building = ef.get_building(idfname)
 
 
 # Helper function to find columns dynamically (used in both parts)
-def get_columns(df, zone_name_part):
-    # Find aPMV value column
-    apmv_col = [i for i in df.columns if 'EMS:aPMV' in i and zone_name_part in i and 'Setpoint' not in i][0]
-    # Find cooling setpoint column (no tolerance)
-    cool_sp_col = [i for i in df.columns if 'EMS:aPMV Cooling Setpoint No Tolerance' in i and zone_name_part in i][0]
-    # Find heating setpoint column (no tolerance)
-    heat_sp_col = [i for i in df.columns if 'EMS:aPMV Heating Setpoint No Tolerance' in i and zone_name_part in i][0]
-
-    return apmv_col, cool_sp_col, heat_sp_col
+# def get_columns(df, zone_name_part):
+#     # Find aPMV value column
+#     apmv_col = [i for i in df.columns if 'EMS:aPMV' in i and zone_name_part in i and 'Setpoint' not in i][0]
+#     # Find cooling setpoint column (no tolerance)
+#     cool_sp_col = [i for i in df.columns if 'EMS:aPMV Cooling Setpoint No Tolerance' in i and zone_name_part in i][0]
+#     # Find heating setpoint column (no tolerance)
+#     heat_sp_col = [i for i in df.columns if 'EMS:aPMV Heating Setpoint No Tolerance' in i and zone_name_part in i][0]
+#
+#     return apmv_col, cool_sp_col, heat_sp_col
 
 # Create a copy of the building for the baseline simulation
 building_pmv = ef.get_building(idfname)
 # apmv.add_vrf_system(building=building_pmv, SupplyAirTempInputMethod='temperature difference')
 #Continuar aqui: re-simular con ocupación constante siempre ocupado
-apmv.set_zones_always_occupied(building=building_pmv)
+# apmv.set_zones_always_occupied(building=building_pmv)
 
-# saveas_name = idfname.split('.idf')[0] + '_VRFsystem_script.idf'
+# saveas_name = idfname.split('.idf')[0] + '_debugging.idf'
 # building_pmv.saveas(saveas_name)
 
+# zones = apmv.get_available_target_names(building=building_pmv)
+# zone_dict = apmv.get_input_template_dictionary(building=building_pmv)
+# zones
+
+
+
+##
+csp_dict = {'Floor_1 Residential Living Occupants': 0.6, 'Floor_2 Residential Living Occupants': 0.3}
 # Apply setpoints with coefficients set to 0
 building_with_pmv = apmv.apply_apmv_setpoints(
     building=building_pmv,
     adap_coeff_cooling=0,
     adap_coeff_heating=0,
-    pmv_cooling_sp=0.5,
+    pmv_cooling_sp=csp_dict,
+    # pmv_cooling_sp=0.5,
     pmv_heating_sp=-0.5,
     cooling_season_start='01/04',  # April 1st
     cooling_season_end='01/10',    # October 1st
@@ -70,11 +85,16 @@ building_with_pmv = apmv.apply_apmv_setpoints(
     # tolerance_cooling_sp_heating_season=-0.15
 )
 
+saveas_name = idfname.split('.idf')[0] + '_debugging'+wip+'.idf'
+building_with_pmv.savecopy(saveas_name)
+
 # building_with_pmv.savecopy('TestModel_TestResidentialUnit_v01_VRF_2_with_EMS.idf')
 
 ## Run simulation
 
-output_dir_base = 'sim_results_pmv_wip_always_occupied_air-veloc-mod'
+# output_dir_base = 'sim_results_pmv_wip_always_occupied_air-veloc-mod_unified_zonelist'
+# output_dir_base = 'sim_results_pmv_wip_aljarafe_zonelist'
+output_dir_base = 'results_'+idfname.split('.idf')[0]+wip
 print(f"Running baseline simulation in: {output_dir_base}...")
 
 ep.run_building(
@@ -105,3 +125,23 @@ print("Baseline simulation finished.")
 # plt.xlabel('Hours of the Year')
 # plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.12), frameon=True)
 # plt.show()
+
+##
+# import besos.eppy_funcs as ef
+# import besos.eplus_funcs as ep
+# epwfile = "Seville.epw"
+#
+# # building = ef.get_building("TestModel_TestResidentialUnit_v01_VRF_2_air-veloc-mod_zonelist-and-spacelist.idf")
+# building = ef.get_building("TestModel_ALJARAFE CENTER_mod_zonelist.idf")
+#
+#
+# # output_dir_base = 'sim_results_pmv_wip_always_occupied_air-veloc-mod_unified_zonelist'
+# output_dir_base = 'sim_results_pmv_wip_aljarafe_zonelist'
+# print(f"Running baseline simulation in: {output_dir_base}...")
+#
+# ep.run_building(
+#     building=building,
+#     out_dir=output_dir_base,
+#     epw=epwfile
+# )
+# print("Baseline simulation finished.")
